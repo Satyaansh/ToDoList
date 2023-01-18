4492154c55ad73fddcdb68cd512f526aa294ae07
Satyaansh <satyaansh@Satyaanshs-MacBook-Pro.local>
2023-01-18 23:23:58 +0530

    initial commit

---
+++ src/test/java/com/management/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
