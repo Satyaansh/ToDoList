a34ec409e98c48a64a488cc97fb16f979e04e393
Satyaansh <satyanshasthana@gmail.com>
2023-01-23 22:16:43 +0530

    Edit ToDoController

--- src/main/java/com/management/todo/controller/ToDoController.java
+++ src/main/java/com/management/todo/controller/ToDoController.java
@@ -1,3 +1,10 @@
+package com.management.todo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ToDoController {
+    
     
 }
