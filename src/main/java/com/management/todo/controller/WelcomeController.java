f27b05408d4e3d7a94b5f27ae2edee151bba114a
Satyaansh <satyanshasthana@gmail.com>
2023-02-02 22:09:57 +0530

    Create Welcome page controller

---
+++ src/main/java/com/management/todo/controller/WelcomeController.java
@@ -0,0 +1,34 @@
+package com.management.todo.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class WelcomeController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public  String showWelcomePage(ModelMap model) {
+
+        model.put("name", getLoggedinUserName());
+        return "welcome";
+
+    }
+
+    private String getLoggedinUserName() {
+
+        Object principal = SecurityContextHolder.getContext()
+                                                .getAuthentication()
+                                                .getPrincipal();
+
+        if(principal instanceof UserDetails) {
+
+            return ((UserDetails) principal).getUsername();
+
+        }
+
+        return principal.toString();
+
+    }
+    
+}
